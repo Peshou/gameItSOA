30c613b2d0950a1ecc578df02f15e7c702c02b63
Peshou <stefan.pesik@yahoo.com>
2017-04-05 21:12:55 +0200

    Initial commit

---
+++ src/main/java/com/gameit/GameitSoaApplication.java
@@ -0,0 +1,12 @@
+package com.gameit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameitSoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameitSoaApplication.class, args);
+	}
+}
