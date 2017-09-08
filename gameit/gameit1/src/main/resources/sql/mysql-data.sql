83f8ecb6d5e057ee43c06244d126625c03a0722b
Peshou <stefan.pesik@yahoo.com>
2017-09-09 01:24:35 +0200

    Remove unused project

--- gameit/gameit1/src/main/resources/sql/mysql-data.sql
+++
@@ -1,6 +0,0 @@
-# INSERT INTO appuser (id, email, password) VALUES ('admin','admin@example.com', '$2a$10$Uym.7DZ1fhr11ZvL52gO8eiE9uJx/.y3tReM2CrNITjci1Fx472wq');
-# INSERT INTO appuser (id, email, password) VALUES ('user', 'user@example.com', '$2a$10$a/gW1h7VNQN4zW0yB1CmbexqkZeirJ2j.sxyL9I7dwc/hwHdHmBpe');
-#
-# INSERT INTO authority (name) VALUES ('ROLE_BUYER');
-# INSERT INTO authority (name) VALUES ('ROLE_ADMIN');
-# INSERT INTO authority (name) VALUES ('ROLE_SELLER');
